c36e1b942f548dc851558b511e76a1c7b3c68b47
Rajpreet0 <87982307+Rajpreet0@users.noreply.github.com>
2022-07-25 01:51:43 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        nunito: ['Nunito', 'sans-serif']
+        nunito: ['Nunito', 'sans-serif'],
+        dancing: ['Dancing Script', 'cursive'],
       }
     },
   },
